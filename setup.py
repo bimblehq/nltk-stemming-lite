8c483cdb89eecebbc33901cf3d1c8f0562774da1
Tomi <dev.tomi33@gmail.com>
2020-03-20 14:24:32 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='ntlk-stemming-lite',
+    version='1.0.0',
+    description='Stemming from the NTLK library usable on AppEngine',
+    url='https://github.com/bimblehq/nltk-stemming-lite',
+    author='Tomi Novak',
+    author_email='tomi@bimble.com',
+    license='MIT',
+    packages=['ntlk-stemming-lite'],
+    include_package_data=False,
+    zip_safe=False
+)
